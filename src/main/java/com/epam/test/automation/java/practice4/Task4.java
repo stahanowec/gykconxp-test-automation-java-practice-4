907a07ed0fb34a2314c4818aef3bf7d286d4b2cf
Hurin Serhiy <sgurin@ukr.net>
2021-10-05 17:32:29 +0300

    Task 4. Fixed p3

--- src/main/java/com/epam/test/automation/java/practice4/Task4.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task4.java
@@ -6,7 +6,7 @@ public class Task4 {
     }
 
     public static double sumGeometricElements(int a1, double t, int alim) {
-        if (alim > 0 && a1 > alim && t > 0) {
+        if (alim > 0 && a1 > alim && t > 0 && t < 1 && alim !=0) {
             double sum = 0;
             double element = a1;
             for (int i = 0; i < alim; i++) {
@@ -16,11 +16,8 @@ public class Task4 {
                     break;
                 }
             }
-
-            return sum;
-
+            return Math.round(sum);
         }
-
         throw new IllegalArgumentException();
     }
 }
