75277887ce9ea33af0d550807b307f3d718d5d78
Hurin Serhiy <sgurin@ukr.net>
2021-10-05 17:55:55 +0300

    Task 4. Fixed p3

--- src/main/java/com/epam/test/automation/java/practice4/Task4.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task4.java
@@ -6,10 +6,10 @@ public class Task4 {
     }
 
     public static double sumGeometricElements(int a1, double t, int alim) {
-        if (alim > 0 && a1 > alim && t > 0 && t < 1 && alim !=0) {
+        if (alim >= 0 && a1 > alim && t > 0 && t < 1) {
             double sum = 0;
             double element = a1;
-            for (int i = 0; i < alim; i++) {
+            while (element>0.0000001) {
                 sum = sum + element;
                 element=  element * t;
                 if (element < alim) {
